1a404726a64ba27257c714f9a5fed201584d9206
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-05-18 18:21:34 +0200

    dap-ui layout

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -82,34 +82,16 @@ dapui.setup {
   layouts = {
     {
       elements = {
-        {
-          id = 'scopes',
-          size = 0.25,
-        },
-        {
-          id = 'breakpoints',
-          size = 0.25,
-        },
-        {
-          id = 'stacks',
-          size = 0.25,
-        },
-        {
-          id = 'watches',
-          size = 0.25,
-        },
+        { id = 'stacks', size = 0.5 },
+        { id = 'console', size = 0.5 },
       },
       position = 'left',
       size = 40,
     },
     {
-      elements = { {
-        id = 'repl',
-        size = 0.5,
-      }, {
-        id = 'console',
-        size = 0.5,
-      } },
+      elements = {
+        { id = 'scopes', size = 1.0 },
+      },
       position = 'bottom',
       size = 10,
     },
