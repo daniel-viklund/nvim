2b9abd69ac44c894b57f1f690b6c9e209320995d
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-05-28 13:22:14 +0200

    close all buffers mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,3 +7,11 @@ require("mini.bufremove").setup()
 vim.keymap.set("n", "<leader>bd", function()
   require("mini.bufremove").delete(0, false)
 end)
+vim.keymap.set('n', '<leader>bla', function()
+  local bufs = vim.api.nvim_list_bufs()
+  for _, buf in ipairs(bufs) do
+    if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted then
+      require("mini.bufremove").delete(buf, false)
+    end
+  end
+end)
