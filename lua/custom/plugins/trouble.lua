c9a7b2361a881f8563c294ee9b14efe5cc4e446a
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-06-04 15:58:04 +0200

    trouble mappings

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -16,7 +16,7 @@ require('trouble').setup {
 }
 
 -- keymaps
-vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>',                          { desc = 'Diagnostics (Trouble)' })
+vim.keymap.set('n', '<leader>q', '<cmd>Trouble diagnostics toggle<cr>',                          { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',             { desc = 'Buffer diagnostics (Trouble)' })
 vim.keymap.set('n', '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>',                  { desc = 'Symbols (Trouble)' })
 vim.keymap.set('n', '<leader>cl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',   { desc = 'LSP (Trouble)' })
