4529ed686a86dc80f5e30e713479f3c718e8750c
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-06-04 12:37:43 +0200

    clangd

--- init.lua
+++ init.lua
@@ -720,7 +720,7 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
-    -- clangd = {},
+    clangd = {},
     -- gopls = {},
     pyright = {},
     rust_analyzer = {},
