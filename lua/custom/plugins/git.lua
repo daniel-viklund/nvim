3ee7708b8f5503c8bd85e3777c5440d903240215
daniel-viklund <166545775+daniel-viklund@users.noreply.github.com>
2026-06-16 16:22:13 +0200

    git diff and hunks

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,4 +5,30 @@ vim.g.lazygit_floating_window_scaling_factor = 1
 vim.g.lazygit_floating_window_winblend = 0
 
 -- keymap
-vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
+vim.keymap.set('n', '<leader>fg', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
+
+-- gitsigns: inline hunks, staging, blame
+vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
+-- configuration
+require('gitsigns').setup {
+  current_line_blame = true,
+}
+-- keymaps
+local gs = require('gitsigns')
+vim.keymap.set('n', ']h', function() gs.nav_hunk('next') end, { desc = 'Next hunk' })
+vim.keymap.set('n', '[h', function() gs.nav_hunk('prev') end, { desc = 'Prev hunk' })
+vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage hunk' })
+vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset hunk' })
+vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview hunk' })
+-- vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'Blame line' })
+-- vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'Diff this file' })
+
+
+-- diffview: review diffs across files / revisions
+vim.pack.add { 'https://github.com/sindrets/diffview.nvim' }
+-- configuration
+require('diffview').setup {}
+-- keymaps
+vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>', { desc = 'Diffview: open' })
+vim.keymap.set('n', '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', { desc = 'Diffview: file history' })
+vim.keymap.set('n', '<leader>gc', '<cmd>DiffviewClose<cr>', { desc = 'Diffview: close' })
