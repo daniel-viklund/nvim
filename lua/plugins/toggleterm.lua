78609a5eabae04cce9f77518d348152ea77e4524
daniel-viklund <daniel.viklund01@gmail.com>
2026-09-24 15:19:48 +0200

    float terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 vim.pack.add { 'https://github.com/akinsho/toggleterm.nvim' }
 require('toggleterm').setup {
   open_mapping = [[<C-\>]],
-  direction = 'tab',
+  direction = 'float',
   float_opts = {
     border = 'curved',
   },
