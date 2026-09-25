73d70fe73b8e91a6c1cf154dc8a2d5d2fd8fdd99
daniel-viklund <daniel.viklund01@gmail.com>
2026-09-25 09:03:02 +0200

    fix: collision with toggleterm mapping

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,2 +1,9 @@
 vim.pack.add({ "https://github.com/christoomey/vim-tmux-navigator" })
 
+vim.g.tmux_navigator_no_mappings = 1
+
+vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { silent = true })
+vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { silent = true })
+vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { silent = true })
+vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { silent = true })
+-- deliberately not mapping <C-\> so toggleterm keeps it
